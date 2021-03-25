c417c0b264a79d4330ebe2f309bc025cdf684971
angelicavc7 <angelicavillar_c@live.com>
2021-03-24 17:26:49 -0700

    new commit

---
+++ script/script.js
@@ -0,0 +1,41 @@
+
+
+var generateBtn = document.querySelector("#generate")
+var alpha = "abcdefghijklmnopqrstuvwxyz".split("")
+var number = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+var 
+
+
+function generatePassword(){
+//your code here
+
+//create global variables
+    //passwordlength
+    //lowercase array
+    //uppercase array
+    //special characters array
+    //numbers array
+    //possible characters array
+    //variables for responses to "confirms/prompts"
+    //final password function
+
+
+//display a prompt asking user for password length
+// match edges cases = is a number && ls > 0, <120
+//ensure that they answered true to each unconfirmed
+
+
+//display confines for all different charater types (listed in readme)
+//check if the user confirms on any of the boolean variables that were created, add that array to the possible characters array 
+
+//create a variable that wil hold the final password
+
+//want to create a loop for as long as passwordlength
+    //for each iteration, select a random character out of the possible characters array and add it the final password variable
+
+    //return the final password from the generation password function
+
+
+
+return "extra secret password"
+}
